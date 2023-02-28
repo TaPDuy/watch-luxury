99c8e78b35e90a13ccf5b1886bbcb3aa37d69b60
Tu_Dev <tuvippro6868@gmail.com>
2023-02-28 23:56:09 +0700

    Giao dien Dang Ki

---
+++ app/src/main/java/nhom9/watchluxury/Register.java
@@ -0,0 +1,14 @@
+package nhom9.watchluxury;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
