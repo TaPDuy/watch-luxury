47db26b2dacafd2ad40810b009e85d5ad166cb6f
TaPDuy <tpduy2001@gmail.com>
2023-03-03 17:30:21 +0700

    Change from firebase to retrofit login

--- app/src/main/java/nhom9/watchluxury/Login.java
+++ app/src/main/java/nhom9/watchluxury/Login.java
@@ -2,10 +2,13 @@ package nhom9.watchluxury;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,11 +22,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import nhom9.watchluxury.data.model.LoginInfo;
+import nhom9.watchluxury.data.model.LoginResponse;
+import nhom9.watchluxury.data.remote.APIUtils;
+import nhom9.watchluxury.data.remote.service.AuthService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class Login extends AppCompatActivity {
     private EditText accountEdit , passEdit ;
     private Button btnlogin , btnregister ;
     private FirebaseAuth mAuth ;
 
+    private AuthService authService;
 
 
     @Override
@@ -35,6 +47,7 @@ public class Login extends AppCompatActivity {
         accountEdit = findViewById(R.id.inAccount);
         passEdit = findViewById(R.id.inPassLogin);
 
+        authService = APIUtils.getAuthenticationService();
 
         btnlogin =  (Button) findViewById(R.id.btnLogin);
         btnregister = (Button)findViewById(R.id.btnRegister);
@@ -58,35 +71,59 @@ public class Login extends AppCompatActivity {
 
 
     private void login() {
+        Context context = this;
         String account , pass ;
         account = accountEdit.getText().toString();
         pass = passEdit.getText().toString();
         if(TextUtils.isEmpty(account)){
-            Toast.makeText(this, "Please! fill account ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Please! fill account ", Toast.LENGTH_SHORT).show();
             return;
         }
         if(TextUtils.isEmpty(pass)){
-            Toast.makeText(this, "Please! fill password to login", Toast.LENGTH_SHORT).show();
+            Toast.makeText(context, "Please! fill password to login", Toast.LENGTH_SHORT).show();
             return;
         }
-        mAuth.signInWithEmailAndPassword(account, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+        authService.login(new LoginInfo(account, pass)).enqueue(new Callback<LoginResponse>() {
             @Override
-
-
-
-                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-                    if(task.isSuccessful()){
-                        Toast.makeText(getApplicationContext(), "Welcome to login successfull", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(Login.this , HomePage.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(getApplicationContext(), "Sorry! Login unsuccessful", Toast.LENGTH_SHORT).show();
-
-                    }
+            public void onResponse(@NonNull Call<LoginResponse> call, @NonNull Response<LoginResponse> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(context, "Login successful!", Toast.LENGTH_LONG).show();
+                    // Decode and store tokens
+                    Log.d("LoginActivity", response.body().toString());
+                } else if (response.code() >= 400 && response.code() < 500) {
+                    Toast.makeText(context, "Username or password is not correct", Toast.LENGTH_LONG).show();
+                    Log.d("LoginActivity", "Couldn't login (401)");
+                    Log.d("LoginActivity", call.toString());
+                    Log.d("LoginActivity", response.message());
+                } else {
+                    Toast.makeText(context, "Oops, something went wrong!", Toast.LENGTH_LONG).show();
+                    Log.d("LoginActivity", "Couldn't login (" + response.code() + ")");
+                    Log.d("LoginActivity", call.toString());
                 }
+            }
 
-
-
+            @Override
+            public void onFailure(@NonNull Call<LoginResponse> call, @NonNull Throwable t) {
+                Toast.makeText(context, "Oops, something went wrong!", Toast.LENGTH_LONG).show();
+                Log.d("LoginActivity", "Couldn't login");
+                Log.d("LoginActivity", call.toString());
+                Log.d("LoginActivity", t.getMessage());
+            }
         });
+
+//        mAuth.signInWithEmailAndPassword(account, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+//                if(task.isSuccessful()){
+//                    Toast.makeText(getApplicationContext(), "Welcome to login successfull", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(Login.this , HomePage.class);
+//                    startActivity(intent);
+//                }else{
+//                    Toast.makeText(getApplicationContext(), "Sorry! Login unsuccessful", Toast.LENGTH_SHORT).show();
+//
+//                }
+//            }
+//        });
     }
 }
