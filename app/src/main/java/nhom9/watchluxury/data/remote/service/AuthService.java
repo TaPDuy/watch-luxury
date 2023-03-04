7b6d638a84e0313fc22fa17b8f3a988db47def9c
TaPDuy <tpduy2001@gmail.com>
2023-03-04 17:35:48 +0700

    Add register new user

--- app/src/main/java/nhom9/watchluxury/data/remote/service/AuthService.java
+++ app/src/main/java/nhom9/watchluxury/data/remote/service/AuthService.java
@@ -2,6 +2,7 @@ package nhom9.watchluxury.data.remote.service;
 
 import nhom9.watchluxury.data.model.LoginInfo;
 import nhom9.watchluxury.data.model.LoginResponse;
+import nhom9.watchluxury.data.model.User;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -10,4 +11,7 @@ public interface AuthService {
 
     @POST("login/")
     Call<LoginResponse> login(@Body LoginInfo info);
+
+    @POST("register/")
+    Call<User> register(@Body User user);
 }
