c12bef00d0c6e16527d36a7cf6a5a1fd0a5ad22b
TaPDuy <tpduy2001@gmail.com>
2023-03-24 22:31:57 +0700

    Add rxjava adapter to retrofit

--- app/src/main/java/nhom9/watchluxury/data/remote/RetrofitClient.java
+++ app/src/main/java/nhom9/watchluxury/data/remote/RetrofitClient.java
@@ -1,6 +1,8 @@
 package nhom9.watchluxury.data.remote;
 
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.moshi.MoshiConverterFactory;
 
@@ -12,13 +14,18 @@ public class RetrofitClient {
 
         if (retrofit == null) {
 
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.level(HttpLoggingInterceptor.Level.BODY);
+
             OkHttpClient okClient = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
                     .authenticator(new TokenAuthenticator())
                     .build();
 
             retrofit = new Retrofit.Builder()
                     .baseUrl(baseUrl)
                     .addConverterFactory(MoshiConverterFactory.create())
+                    .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                     .client(okClient)
                     .build();
         }
