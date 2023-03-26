a3721f8cf37c444af5b78940ff40c18f9b4a6f98
TaPDuy <tpduy2001@gmail.com>
2023-03-26 15:26:37 +0700

    Add product info page

---
+++ app/src/main/java/nhom9/watchluxury/activity/ProductInfoActivity.java
@@ -0,0 +1,16 @@
+package nhom9.watchluxury.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nhom9.watchluxury.R;
+
+public class ProductInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_info);
+    }
+}
---
+++ app/src/main/java/nhom9/watchluxury/viewmodel/ProductInfoViewModel.java
@@ -0,0 +1,4 @@
+package nhom9.watchluxury.viewmodel;
+
+public class ProductInfoViewModel {
+}
