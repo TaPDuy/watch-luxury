9ab51aa74f295459c3f996debfec734de34a3aa5
TaPDuy <tpduy2001@gmail.com>
2023-04-09 16:02:54 +0700

    Fix typo

--- app/src/main/java/nhom9/watchluxury/data/remote/service/FavoriteService.java
+++ app/src/main/java/nhom9/watchluxury/data/remote/service/FavoriteService.java
@@ -7,21 +7,24 @@ import nhom9.watchluxury.data.model.Product;
 import nhom9.watchluxury.data.remote.model.APIResource;
 import nhom9.watchluxury.data.remote.model.FavoriteRequest;
 import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface FavoriteService {
 
     @GET("users/{id}/favorites/")
-    Single<APIResource<List<Product>>> getFavorites(@Path("id") int id, @Header("Authorization") String accessToken);
+    Single<APIResource<List<Product>>> getUserFavorites(@Path("id") int id, @Header("Authorization") String accessToken);
 
-    @POST("favorite/")
+    @GET("/favorites/")
+    Single<APIResource<List<FavoriteRequest>>> getFavorites(@Query("user") int userID, @Query("product") int productID, @Header("Authorization") String accessToken);
+
+    @POST("favorites/")
     Single<APIResource<FavoriteRequest>> addFavorite(@Body FavoriteRequest request, @Header("Authorization") String accessToken);
 
-    @HTTP(method = "DELETE", path = "favorite/", hasBody = true)
+    @HTTP(method = "DELETE", path = "favorites/", hasBody = true)
     Single<APIResource<FavoriteRequest>> removeFavorite(@Body FavoriteRequest request, @Header("Authorization") String accessToken);
 }
